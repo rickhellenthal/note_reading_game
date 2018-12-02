fd2676eee2896e39c482a389083836ddc6eb2b12
rickhellenthal <rickhellenthal@hotmail.com>
2018-12-02 19:29:10 +0100

    Added lefthand functionality

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -29,56 +29,7 @@ namespace Controller
 	 */
         void Start ()
         {
-            const string path = "Images/Notes/RightHand/";
-        
-            _noteList.Add(new Note("R01_C_Note", "C_Note", Resources.Load<Sprite>(path + "R01_C_Note")));
-            _noteList.Add(new Note("R02_D_Note", "D_Note", Resources.Load<Sprite>(path + "R02_D_Note")));
-            _noteList.Add(new Note("R03_E_Note", "E_Note", Resources.Load<Sprite>(path + "R03_E_Note")));
-            _noteList.Add(new Note("R04_F_Note", "F_Note", Resources.Load<Sprite>(path + "R04_F_Note")));
-            _noteList.Add(new Note("R05_G_Note", "G_Note", Resources.Load<Sprite>(path + "R05_G_Note")));
-            _noteList.Add(new Note("R06_A_Note", "A_Note", Resources.Load<Sprite>(path + "R06_A_Note")));
-            _noteList.Add(new Note("R07_B_Note", "B_Note", Resources.Load<Sprite>(path + "R07_B_Note")));
-            _noteList.Add(new Note("R08_C1_Note", "C_Note", Resources.Load<Sprite>(path + "R08_C1_Note")));
-            _noteList.Add(new Note("R09_D1_Note", "D_Note", Resources.Load<Sprite>(path + "R09_D1_Note")));
-            _noteList.Add(new Note("R10_E1_Note", "E_Note", Resources.Load<Sprite>(path + "R10_E1_Note")));
-            _noteList.Add(new Note("R11_F1_Note", "F_Note", Resources.Load<Sprite>(path + "R11_F1_Note")));
-            _noteList.Add(new Note("R12_G1_Note", "G_Note", Resources.Load<Sprite>(path + "R12_G1_Note")));
-            _noteList.Add(new Note("R13_A1_Note", "A_Note", Resources.Load<Sprite>(path + "R13_A1_Note")));
-
-            if (CrossSceneInfo.IncludeSharpNotes)
-            {
-                _noteList.Add(new Note("R01_Cs_Note", "Cs_Note", Resources.Load<Sprite>(path + "R01_Cs_Note")));
-                _noteList.Add(new Note("R02_Ds_Note", "Ds_Note", Resources.Load<Sprite>(path + "R02_Ds_Note")));
-                _noteList.Add(new Note("R03_Es_Note", "F_Note", Resources.Load<Sprite>(path + "R03_Es_Note")));
-                _noteList.Add(new Note("R04_Fs_Note", "Fs_Note", Resources.Load<Sprite>(path + "R04_Fs_Note")));
-                _noteList.Add(new Note("R05_Gs_Note", "Gs_Note", Resources.Load<Sprite>(path + "R05_Gs_Note")));
-                _noteList.Add(new Note("R06_As_Note", "As_Note", Resources.Load<Sprite>(path + "R06_As_Note")));
-                _noteList.Add(new Note("R07_Bs_Note", "C_Note", Resources.Load<Sprite>(path + "R07_Bs_Note")));
-                _noteList.Add(new Note("R08_C1s_Note", "Cs_Note", Resources.Load<Sprite>(path + "R08_C1s_Note")));
-                _noteList.Add(new Note("R09_D1s_Note", "Ds_Note", Resources.Load<Sprite>(path + "R09_D1s_Note")));
-                _noteList.Add(new Note("R10_E1s_Note", "F_Note", Resources.Load<Sprite>(path + "R10_E1s_Note")));
-                _noteList.Add(new Note("R11_F1s_Note", "Fs_Note", Resources.Load<Sprite>(path + "R11_F1s_Note")));
-                _noteList.Add(new Note("R12_G1s_Note", "Gs_Note", Resources.Load<Sprite>(path + "R12_G1s_Note")));
-                _noteList.Add(new Note("R13_A1s_Note", "As_Note", Resources.Load<Sprite>(path + "R13_A1s_Note")));
-            }
-
-            if (CrossSceneInfo.IncludeFlatNotes)
-            {
-                _noteList.Add(new Note("R01_Cb_Note", "B_Note", Resources.Load<Sprite>(path + "R01_Cb_Note")));
-                _noteList.Add(new Note("R02_Db_Note", "Cs_Note", Resources.Load<Sprite>(path + "R02_Db_Note")));
-                _noteList.Add(new Note("R03_Eb_Note", "Ds_Note", Resources.Load<Sprite>(path + "R03_Eb_Note")));
-                _noteList.Add(new Note("R04_Fb_Note", "E_Note", Resources.Load<Sprite>(path + "R04_Fb_Note")));
-                _noteList.Add(new Note("R05_Gb_Note", "Fs_Note", Resources.Load<Sprite>(path + "R05_Gb_Note")));
-                _noteList.Add(new Note("R06_Ab_Note", "Gs_Note", Resources.Load<Sprite>(path + "R06_Ab_Note")));
-                _noteList.Add(new Note("R07_Bb_Note", "As_Note", Resources.Load<Sprite>(path + "R07_Bb_Note")));
-                _noteList.Add(new Note("R08_C1b_Note", "B_Note", Resources.Load<Sprite>(path + "R08_C1b_Note")));
-                _noteList.Add(new Note("R09_D1b_Note", "Cs_Note", Resources.Load<Sprite>(path + "R09_D1b_Note")));
-                _noteList.Add(new Note("R10_E1b_Note", "Ds_Note", Resources.Load<Sprite>(path + "R10_E1b_Note")));
-                _noteList.Add(new Note("R11_F1b_Note", "E_Note", Resources.Load<Sprite>(path + "R11_F1b_Note")));
-                _noteList.Add(new Note("R12_G1b_Note", "Fs_Note", Resources.Load<Sprite>(path + "R12_G1b_Note")));
-                _noteList.Add(new Note("R13_A1b_Note", "Gs_Note", Resources.Load<Sprite>(path + "R13_A1b_Note")));
-            }
-
+            LoadNotes();
             NextNote();
         }
 
@@ -168,6 +119,125 @@ namespace Controller
         {
             CrossSceneInfo.ScoreAchieved = Score.GetScore();
             SceneController.LoadEndGame();
+        }
+        
+        public void LoadNotes() {
+            const string leftPath = "Images/Notes/LeftHand/";
+            const string rightPath = "Images/Notes/RightHand/";
+            
+            if (CrossSceneInfo.ClefSetting == 0 || CrossSceneInfo.ClefSetting == 2)
+            {
+                _noteList.Add(new Note("R01_C_Note", "C_Note", Resources.Load<Sprite>(rightPath + "R01_C_Note")));
+                _noteList.Add(new Note("R02_D_Note", "D_Note", Resources.Load<Sprite>(rightPath + "R02_D_Note")));
+                _noteList.Add(new Note("R03_E_Note", "E_Note", Resources.Load<Sprite>(rightPath + "R03_E_Note")));
+                _noteList.Add(new Note("R04_F_Note", "F_Note", Resources.Load<Sprite>(rightPath + "R04_F_Note")));
+                _noteList.Add(new Note("R05_G_Note", "G_Note", Resources.Load<Sprite>(rightPath + "R05_G_Note")));
+                _noteList.Add(new Note("R06_A_Note", "A_Note", Resources.Load<Sprite>(rightPath + "R06_A_Note")));
+                _noteList.Add(new Note("R07_B_Note", "B_Note", Resources.Load<Sprite>(rightPath + "R07_B_Note")));
+                _noteList.Add(new Note("R08_C1_Note", "C_Note", Resources.Load<Sprite>(rightPath + "R08_C1_Note")));
+                _noteList.Add(new Note("R09_D1_Note", "D_Note", Resources.Load<Sprite>(rightPath + "R09_D1_Note")));
+                _noteList.Add(new Note("R10_E1_Note", "E_Note", Resources.Load<Sprite>(rightPath + "R10_E1_Note")));
+                _noteList.Add(new Note("R11_F1_Note", "F_Note", Resources.Load<Sprite>(rightPath + "R11_F1_Note")));
+                _noteList.Add(new Note("R12_G1_Note", "G_Note", Resources.Load<Sprite>(rightPath + "R12_G1_Note")));
+                _noteList.Add(new Note("R13_A1_Note", "A_Note", Resources.Load<Sprite>(rightPath + "R13_A1_Note")));
+    
+                if (CrossSceneInfo.IncludeSharpNotes)
+                {
+                    _noteList.Add(new Note("R01_Cs_Note", "Cs_Note", Resources.Load<Sprite>(rightPath + "R01_Cs_Note")));
+                    _noteList.Add(new Note("R02_Ds_Note", "Ds_Note", Resources.Load<Sprite>(rightPath + "R02_Ds_Note")));
+                    _noteList.Add(new Note("R03_Es_Note", "F_Note", Resources.Load<Sprite>(rightPath + "R03_Es_Note")));
+                    _noteList.Add(new Note("R04_Fs_Note", "Fs_Note", Resources.Load<Sprite>(rightPath + "R04_Fs_Note")));
+                    _noteList.Add(new Note("R05_Gs_Note", "Gs_Note", Resources.Load<Sprite>(rightPath + "R05_Gs_Note")));
+                    _noteList.Add(new Note("R06_As_Note", "As_Note", Resources.Load<Sprite>(rightPath + "R06_As_Note")));
+                    _noteList.Add(new Note("R07_Bs_Note", "C_Note", Resources.Load<Sprite>(rightPath + "R07_Bs_Note")));
+                    _noteList.Add(new Note("R08_C1s_Note", "Cs_Note", Resources.Load<Sprite>(rightPath + "R08_C1s_Note")));
+                    _noteList.Add(new Note("R09_D1s_Note", "Ds_Note", Resources.Load<Sprite>(rightPath + "R09_D1s_Note")));
+                    _noteList.Add(new Note("R10_E1s_Note", "F_Note", Resources.Load<Sprite>(rightPath + "R10_E1s_Note")));
+                    _noteList.Add(new Note("R11_F1s_Note", "Fs_Note", Resources.Load<Sprite>(rightPath + "R11_F1s_Note")));
+                    _noteList.Add(new Note("R12_G1s_Note", "Gs_Note", Resources.Load<Sprite>(rightPath + "R12_G1s_Note")));
+                    _noteList.Add(new Note("R13_A1s_Note", "As_Note", Resources.Load<Sprite>(rightPath + "R13_A1s_Note")));
+                }
+    
+                if (CrossSceneInfo.IncludeFlatNotes)
+                {
+                    _noteList.Add(new Note("R01_Cb_Note", "B_Note", Resources.Load<Sprite>(rightPath + "R01_Cb_Note")));
+                    _noteList.Add(new Note("R02_Db_Note", "Cs_Note", Resources.Load<Sprite>(rightPath + "R02_Db_Note")));
+                    _noteList.Add(new Note("R03_Eb_Note", "Ds_Note", Resources.Load<Sprite>(rightPath + "R03_Eb_Note")));
+                    _noteList.Add(new Note("R04_Fb_Note", "E_Note", Resources.Load<Sprite>(rightPath + "R04_Fb_Note")));
+                    _noteList.Add(new Note("R05_Gb_Note", "Fs_Note", Resources.Load<Sprite>(rightPath + "R05_Gb_Note")));
+                    _noteList.Add(new Note("R06_Ab_Note", "Gs_Note", Resources.Load<Sprite>(rightPath + "R06_Ab_Note")));
+                    _noteList.Add(new Note("R07_Bb_Note", "As_Note", Resources.Load<Sprite>(rightPath + "R07_Bb_Note")));
+                    _noteList.Add(new Note("R08_C1b_Note", "B_Note", Resources.Load<Sprite>(rightPath + "R08_C1b_Note")));
+                    _noteList.Add(new Note("R09_D1b_Note", "Cs_Note", Resources.Load<Sprite>(rightPath + "R09_D1b_Note")));
+                    _noteList.Add(new Note("R10_E1b_Note", "Ds_Note", Resources.Load<Sprite>(rightPath + "R10_E1b_Note")));
+                    _noteList.Add(new Note("R11_F1b_Note", "E_Note", Resources.Load<Sprite>(rightPath + "R11_F1b_Note")));
+                    _noteList.Add(new Note("R12_G1b_Note", "Fs_Note", Resources.Load<Sprite>(rightPath + "R12_G1b_Note")));
+                    _noteList.Add(new Note("R13_A1b_Note", "Gs_Note", Resources.Load<Sprite>(rightPath + "R13_A1b_Note")));
+                }
+            }
+
+            if (CrossSceneInfo.ClefSetting == 1 || CrossSceneInfo.ClefSetting == 2)
+            {
+                _noteList.Add(new Note("L01_E_Note", "E_Note", Resources.Load<Sprite>(leftPath + "L01_E_Note")));
+                _noteList.Add(new Note("L02_F_Note", "F_Note", Resources.Load<Sprite>(leftPath + "L02_F_Note")));
+                _noteList.Add(new Note("L03_G_Note", "G_Note", Resources.Load<Sprite>(leftPath + "L03_G_Note")));
+                _noteList.Add(new Note("L04_A_Note", "A_Note", Resources.Load<Sprite>(leftPath + "L04_A_Note")));
+                _noteList.Add(new Note("L05_B_Note", "B_Note", Resources.Load<Sprite>(leftPath + "L05_B_Note")));
+                _noteList.Add(new Note("L06_C_Note", "C_Note", Resources.Load<Sprite>(leftPath + "L06_C_Note")));
+                _noteList.Add(new Note("L07_D_Note", "D_Note", Resources.Load<Sprite>(leftPath + "L07_D_Note")));
+                _noteList.Add(new Note("L08_E1_Note", "E_Note", Resources.Load<Sprite>(leftPath + "L08_E1_Note")));
+                _noteList.Add(new Note("L09_F1_Note", "F_Note", Resources.Load<Sprite>(leftPath + "L09_F1_Note")));
+                _noteList.Add(new Note("L10_G1_Note", "G_Note", Resources.Load<Sprite>(leftPath + "L10_G1_Note")));
+                _noteList.Add(new Note("L11_A1_Note", "A_Note", Resources.Load<Sprite>(leftPath + "L11_A1_Note")));
+                _noteList.Add(new Note("L12_B1_Note", "B_Note", Resources.Load<Sprite>(leftPath + "L12_B1_Note")));
+                _noteList.Add(new Note("L13_C1_Note", "C_Note", Resources.Load<Sprite>(leftPath + "L13_C1_Note")));
+
+                if (CrossSceneInfo.IncludeSharpNotes)
+                {
+                    _noteList.Add(new Note("L01_Es_Note", "F_Note", Resources.Load<Sprite>(leftPath + "L01_Es_Note")));
+                    _noteList.Add(new Note("L02_Fs_Note", "Fs_Note", Resources.Load<Sprite>(leftPath + "L02_Fs_Note")));
+                    _noteList.Add(new Note("L03_Gs_Note", "Gs_Note", Resources.Load<Sprite>(leftPath + "L03_Gs_Note")));
+                    _noteList.Add(new Note("L04_As_Note", "As_Note", Resources.Load<Sprite>(leftPath + "L04_As_Note")));
+                    _noteList.Add(new Note("L05_Bs_Note", "C_Note", Resources.Load<Sprite>(leftPath + "L05_Bs_Note")));
+                    _noteList.Add(new Note("L06_Cs_Note", "Cs_Note", Resources.Load<Sprite>(leftPath + "L06_Cs_Note")));
+                    _noteList.Add(new Note("L07_Ds_Note", "Ds_Note", Resources.Load<Sprite>(leftPath + "L07_Ds_Note")));
+                    _noteList.Add(new Note("L08_E1s_Note", "F_Note",
+                        Resources.Load<Sprite>(leftPath + "L08_E1s_Note")));
+                    _noteList.Add(
+                        new Note("L09_F1s_Note", "Fs_Note", Resources.Load<Sprite>(leftPath + "L09_F1s_Note")));
+                    _noteList.Add(
+                        new Note("L10_G1s_Note", "Gs_Note", Resources.Load<Sprite>(leftPath + "L10_G1s_Note")));
+                    _noteList.Add(
+                        new Note("L11_A1s_Note", "As_Note", Resources.Load<Sprite>(leftPath + "L11_A1s_Note")));
+                    _noteList.Add(new Note("L12_B1s_Note", "C_Note",
+                        Resources.Load<Sprite>(leftPath + "L12_B1s_Note")));
+                    _noteList.Add(
+                        new Note("L13_C1s_Note", "Cs_Note", Resources.Load<Sprite>(leftPath + "L13_C1s_Note")));
+                }
+
+                if (CrossSceneInfo.IncludeFlatNotes)
+                {
+                    _noteList.Add(new Note("L01_Eb_Note", "Ds_Note", Resources.Load<Sprite>(leftPath + "L01_Eb_Note")));
+                    _noteList.Add(new Note("L02_Fb_Note", "E_Note", Resources.Load<Sprite>(leftPath + "L02_Fb_Note")));
+                    _noteList.Add(new Note("L03_Gb_Note", "Fs_Note", Resources.Load<Sprite>(leftPath + "L03_Gb_Note")));
+                    _noteList.Add(new Note("L04_Ab_Note", "Gs_Note", Resources.Load<Sprite>(leftPath + "L04_Ab_Note")));
+                    _noteList.Add(new Note("L05_Bb_Note", "As_Note", Resources.Load<Sprite>(leftPath + "L05_Bb_Note")));
+                    _noteList.Add(new Note("L06_Cb_Note", "B_Note", Resources.Load<Sprite>(leftPath + "L06_Cb_Note")));
+                    _noteList.Add(new Note("L07_Db_Note", "Cs_Note", Resources.Load<Sprite>(leftPath + "L07_Db_Note")));
+                    _noteList.Add(
+                        new Note("L08_E1b_Note", "Ds_Note", Resources.Load<Sprite>(leftPath + "L08_E1b_Note")));
+                    _noteList.Add(new Note("L09_F1b_Note", "E_Note",
+                        Resources.Load<Sprite>(leftPath + "L09_F1b_Note")));
+                    _noteList.Add(
+                        new Note("L10_G1b_Note", "Fs_Note", Resources.Load<Sprite>(leftPath + "L10_G1b_Note")));
+                    _noteList.Add(
+                        new Note("L11_A1b_Note", "Gs_Note", Resources.Load<Sprite>(leftPath + "L11_A1b_Note")));
+                    _noteList.Add(
+                        new Note("L12_B1b_Note", "As_Note", Resources.Load<Sprite>(leftPath + "L12_B1b_Note")));
+                    _noteList.Add(new Note("L13_C1b_Note", "B_Note",
+                        Resources.Load<Sprite>(leftPath + "L13_C1b_Note")));
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Model/CrossSceneInfo.cs
+++ Assets/Scripts/Model/CrossSceneInfo.cs
@@ -9,6 +9,7 @@
 		public static int NumberOfAssignmentsCorrect = 0;
 	
 		// Settings
+		public static int ClefSetting = 0; // 0 = Right hand only, 1 = Left hand only, 2 = Both hands
 		public static bool IncludeSharpNotes = true;
 		public static bool IncludeFlatNotes = true;
 
