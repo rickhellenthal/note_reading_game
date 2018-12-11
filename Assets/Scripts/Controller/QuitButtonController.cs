142832f73d7b45ad8034c6f77f55eea816697e23
rickhellenthal <rickhellenthal@hotmail.com>
2018-12-11 16:33:17 +0100

    Improved Quit button in game to also support mouse click

---
+++ Assets/Scripts/Controller/QuitButtonController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+	public class QuitButtonController : MonoBehaviour
+	{
+
+		[SerializeField] private GameController _gameController;
+	
+	
+		private void OnMouseDown()
+		{
+			_gameController.EndGame();
+		}
+	}
+}
